e80baa434449807f5540ab9025fc39952fbcd84e
Ptome2000 <pedro.m.tome16@outlook.com>
2024-01-20 02:17:54 +0000

    Removal of Files

--- Learning-Python/Ch2 - Basics/variables_finished.py
+++ Learning-Python/Ch2 - Basics/variables_finished.py
@@ -13,13 +13,7 @@ mylist = [0, 1, "two", 3.2, False]
 mytuple = (0, 1, 2)
 mydict = {"one" : 1, "two" : 2}
 
-print(myint)
-print(myfloat)
-print(mystr)
-print(mybool)
-print(mylist)
-print(mytuple)
-print(mydict)
+$
 
 # re-declaring a variable works
 myint = "abc"
